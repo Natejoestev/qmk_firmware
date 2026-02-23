ea298c2c8a85e860ed47f481bd6574fbe6cf20bc
Natejoestev <85044853+Natejoestev@users.noreply.github.com>
2026-02-22 16:46:20 -0800

    feat: keymap for customizing capslock indictator

---
+++ keyboards/keychron/v1/ansi_encoder/keymaps/Natejoestev_capsindictator/keymap.c
@@ -0,0 +1,218 @@
+/* Copyright 2021 @ Keychron (https://www.keychron.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+
+#include "color.h"
+#include "rgb_matrix.h"
+#include "host.h"
+#include "eeconfig.h"
+#include "via.h"
+
+
+// clang-format off
+
+enum layers{
+    MAC_BASE,
+    MAC_FN,
+    WIN_BASE,
+    WIN_FN
+};
+
+#define KC_TASK LGUI(KC_TAB)
+#define KC_FLXP LGUI(KC_E)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [MAC_BASE] = LAYOUT_ansi_82(
+        KC_ESC,   KC_BRID,  KC_BRIU,  KC_NO,    KC_NO,    RM_VALD,  RM_VALU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  KC_DEL,             KC_MUTE,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
+        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
+        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
+        KC_LCTL,  KC_LOPT,  KC_LCMD,                                KC_SPC,                                 KC_RCMD,  MO(MAC_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+
+    [MAC_FN] = LAYOUT_ansi_82(
+        _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   _______,            _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
+        RM_TOGG,  RM_NEXT,  RM_VALU,  RM_HUEU,  RM_SATU,  RM_SPDU,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
+        _______,  RM_PREV,  RM_VALD,  RM_HUED,  RM_SATD,  RM_SPDD,  _______,  _______,  _______,  _______,  _______,  _______,              _______,            _______,
+        _______,            _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,  _______,
+        _______,  _______,  _______,                                _______,                                _______,  _______,    _______,  _______,  _______,  _______),
+
+    [WIN_BASE] = LAYOUT_ansi_82(
+        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_DEL,             KC_MUTE,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
+        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
+        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
+        KC_LCTL,  KC_LCMD,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+
+    [WIN_FN] = LAYOUT_ansi_82(
+        _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RM_VALD,  RM_VALU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  _______,            _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
+        RM_TOGG,  RM_NEXT,  RM_VALU,  RM_HUEU,  RM_SATU,  RM_SPDU,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
+        _______,  RM_PREV,  RM_VALD,  RM_HUED,  RM_SATD,  RM_SPDD,  _______,  _______,  _______,  _______,  _______,  _______,              _______,            _______,
+        _______,            _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,  _______,
+        _______,  _______,  _______,                                _______,                                _______,  _______,    _______,  _______,  _______,  _______),
+};
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [MAC_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [MAC_FN]   = { ENCODER_CCW_CW(RM_VALD, RM_VALU)},
+    [WIN_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [WIN_FN]   = { ENCODER_CCW_CW(RM_VALD, RM_VALU)}
+};
+#endif // ENCODER_MAP_ENABLE
+
+/******************
+   Caps Indicator 
+*******************/
+
+// RGB color for the indicator
+rgb_t g_capslight_color = {RGB_GREEN};
+
+// Actualy write the color to the matrix on the (indicator_user callback)
+bool rgb_matrix_indicators_user() {
+    if (host_keyboard_led_state().caps_lock) {
+        rgb_matrix_set_color(
+            CAPS_LOCK_LED_INDEX,
+            g_capslight_color.r,
+            g_capslight_color.g,
+            g_capslight_color.b
+        );
+    }
+    return true;
+}
+
+/* EEPROM/DATA */
+
+// User EEPROM struct
+typedef union {
+    uint32_t raw;
+    struct {
+        hsv_t capslight_color;
+    };
+} custom_config;
+
+custom_config g_custom_config;
+
+// convert g_custom_config.capslight_color (HSV) to RGB
+void update_capslight_color(void) {
+    g_capslight_color = hsv_to_rgb(g_custom_config.capslight_color);
+}
+
+//read from EEPROM on startup
+void keyboard_post_init_user(void) {
+    g_custom_config.raw = eeconfig_read_user();
+
+    update_capslight_color();
+}
+//EEPROM reset
+void eeconfig_init_user(void) {
+  g_custom_config.raw = 0;
+  g_custom_config.capslight_color = (hsv_t){HSV_PURPLE};
+
+  update_capslight_color();
+
+  eeconfig_update_user(g_custom_config.raw);
+}
+
+
+/* VIA set/get callbacks */
+
+// custom "valueID"
+enum via_custom_value {
+    id_custom_caps_brightness = 1,
+    id_custom_caps_color = 2
+};
+
+// Set data to memory
+void custom_config_set_value(uint8_t *data) {
+    // data = [ value_id, value_data ]
+    uint8_t *value_id   = &(data[0]);
+    uint8_t *value_data = &(data[1]);
+
+    switch (*value_id) {
+        case id_custom_caps_brightness: {
+            g_custom_config.capslight_color.v = value_data[0];
+            //scale8(value_data[0], RGB_MATRIX_MAXIMUM_BRIGHTNESS);
+            break;
+        }
+        case id_custom_caps_color: {
+            g_custom_config.capslight_color.h = value_data[0];
+            g_custom_config.capslight_color.s = value_data[1];
+            break;
+        }
+    }
+
+    update_capslight_color();
+}
+// Get data from memory
+void custom_config_get_value(uint8_t *data) {
+    // data = [ value_id, value_data ]
+    uint8_t *value_id   = &(data[0]);
+    uint8_t *value_data = &(data[1]);
+
+    switch (*value_id) {
+        case id_custom_caps_brightness: {
+            *value_data = g_custom_config.capslight_color.v;
+            break;
+        }
+        case id_custom_caps_color: {
+            value_data[0] = g_custom_config.capslight_color.h;
+            value_data[1] = g_custom_config.capslight_color.s;
+            break;
+        }
+    }
+}
+// Save data to EEPROM
+void custom_config_save(void) {
+    eeconfig_update_user(g_custom_config.raw);
+}
+
+// VIA data incoming
+void via_custom_value_command_kb(uint8_t *data, uint8_t length) {
+    // data = [ command_id, channel_id, value_id, value_data ]
+    uint8_t *command_id        = &(data[0]);
+    uint8_t *channel_id        = &(data[1]);
+    uint8_t *value_id_and_data = &(data[2]);
+
+    if (*channel_id == id_custom_channel) {
+        switch ( *command_id ) {
+            case id_custom_set_value: {
+                custom_config_set_value(value_id_and_data);
+                break;
+            }
+            case id_custom_get_value: {
+                custom_config_get_value(value_id_and_data);
+                break;
+            }
+            case id_custom_save: {
+                custom_config_save();
+                break;
+            }
+            default: {
+                *command_id = id_unhandled;
+                break;
+            }
+        }
+        return;
+    }
+    *command_id = id_unhandled;
+
+    // DO NOT call raw_hid_send(data,length) here, let caller do this
+}
